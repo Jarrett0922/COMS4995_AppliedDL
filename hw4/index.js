a6d5b6631169e3252355586363c0be6441cf62ba
jz2977@columbia.edu <Jarrett0922>
2019-04-02 18:59:26 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -10,8 +10,7 @@ const examples = {
   'example1':
       '“There are no crimes and no criminals in these days,” he said, querulously.',
   'example2':
-      '"Oh! my dear Mr. Bennet," as she entered the room, "we have had a most
-delightful evening, a most excellent ball.',
+      '"Oh! my dear Mr. Bennet," as she entered the room, "we have had a most delightful evening, a most excellent ball.',
   'example3':
       'Had it not been for us whalemen, that tract of land would this day perhaps have been in as howling condition as the coast of Labrador.',
   'example4':
